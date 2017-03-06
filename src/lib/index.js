23d87605f42f1c3d30878a86d3ebafa2df65c79d
Sergio Coimbra <sergio.coimbra@jscrambler.com>
2017-03-06 10:48:12 +0000

    Read deprecations from protection response

--- src/lib/index.js
+++ src/lib/index.js
@@ -224,6 +224,12 @@ export default {
       throw new Error('Protection failed');
     }
 
+    if (protection.deprecations) {
+      protection.deprecations.forEach(deprecation => {
+        deprecation && console.warn(`Warning: ${deprecation} is deprecated`);
+      });
+    }
+
     const download = await this.downloadApplicationProtection(client, protectionId);
     errorHandler(download);
     unzip(download, filesDest || destCallback);
